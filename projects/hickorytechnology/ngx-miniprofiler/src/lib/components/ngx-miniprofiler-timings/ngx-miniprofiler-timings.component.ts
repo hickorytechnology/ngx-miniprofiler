ec1ff2f821f97d8a53cd809d0160646c5ad4f3cb
Josh Sullivan <jaysche.sullivan@gmail.com>
2020-10-21 02:27:35 -0400

    fix: 🐛 duplicate custom timing check

--- projects/hickorytechnology/ngx-miniprofiler/src/lib/components/ngx-miniprofiler-timings/ngx-miniprofiler-timings.component.ts
+++ projects/hickorytechnology/ngx-miniprofiler/src/lib/components/ngx-miniprofiler-timings/ngx-miniprofiler-timings.component.ts
@@ -86,7 +86,7 @@ export class NgxMiniProfilerTimingsComponent implements OnInit, OnDestroy {
   public queriesTitle(timing: ITiming, timingName: string): string {
     const dur = this.duration(this.result.CustomTimingStats[timingName].Duration);
     const count = this.result.CustomTimingStats[timingName].Count;
-    const suffix = this.result.HasDuplicateCustomTimings[timingName] ? '; duplicate calls detected!' : '';
+    const suffix = timing.HasDuplicateCustomTimings[timingName] ? '; duplicate calls detected!' : '';
     return `${dur} ms in ${count} ${this.encode(timingName)} call(s)${suffix}`;
   }
 

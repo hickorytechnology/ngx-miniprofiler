c5905c19cff5c0e66b244267042af0ed3a2de6f7
Josh Sullivan <jaysche.sullivan@gmail.com>
2020-10-31 01:50:36 -0400

    fix: 🐛 maxTracesToShow implementation assignment

--- projects/hickorytechnology/ngx-miniprofiler/src/lib/components/ngx-miniprofiler/ngx-miniprofiler.component.ts
+++ projects/hickorytechnology/ngx-miniprofiler/src/lib/components/ngx-miniprofiler/ngx-miniprofiler.component.ts
@@ -48,7 +48,7 @@ export class NgxMiniProfilerComponent implements OnInit, OnDestroy {
                   .sort((x, y) => new Date(x.Started).getTime() - new Date(y.Started).getTime());
 
                 if (this.options.maxTracesToShow > 0) {
-                  formatted.slice(Math.max(formatted.length - this.options.maxTracesToShow, 0));
+                  formatted = formatted.slice(Math.max(formatted.length - this.options.maxTracesToShow, 0));
                 }
 
                 this.profileResults = formatted;
